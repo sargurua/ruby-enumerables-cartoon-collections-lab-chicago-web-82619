ccaf840e9954cbe4ca64f3d0a0e7fd076e6ff640
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 23:16:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(strings)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.map{|n| }
+  strings.index()
 end
