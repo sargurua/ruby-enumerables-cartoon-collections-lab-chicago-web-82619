829074c2d1bb3fe24f58db009cc04cbcb2f35fb1
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 23:28:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,11 +15,5 @@ end
 
 def find_the_cheese(strings)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  item = strings.index{|n| n == }
-  if item
-    return strings[item]
-  else
-    return nil
-  end
+  
 end
